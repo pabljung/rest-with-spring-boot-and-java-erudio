e2f6c89f863f2b3ff556461b6550412b6d689a7b
pabljung <jungvaldespablo@gmail.com>
2026-04-08 17:52:09 -0300

    feat: Test loggers

--- rest-with-spring-boot-and-java-erudio/src/main/java/com/github/pablo/services/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/com/github/pablo/services/PersonServices.java
@@ -3,18 +3,19 @@ package com.github.pablo.services;
 import com.github.pablo.exception.ResourceNotFoundException;
 import com.github.pablo.model.Person;
 import com.github.pablo.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.logging.Logger;
 
 @Service
 public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
-    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    private Logger logger = LoggerFactory.getLogger(PersonServices.class.getName());
 
     @Autowired
     PersonRepository repository;
